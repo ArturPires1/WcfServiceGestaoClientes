c8e6ae90ee1ebf0cc388168b3c5ab5d3d267b229
artur <artur@DESKTOP-EH259IB>
2022-07-08 13:17:19 -0300

    Add project files.

---
+++ WcfServiceGestaoClientes/IService1.cs
@@ -0,0 +1,71 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace WcfServiceGestaoClientes
+{
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string InsertUserRegDetails(RegDetails regdet);
+
+        [OperationContract]
+        DataSet GetUserRegDetails();
+
+        [OperationContract]
+        DataSet FetchUpdatedRecords(RegDetails regdet);
+
+        [OperationContract]
+        string UpdateUserRegDetails(RegDetails regdet);
+
+        [OperationContract]
+        bool DeleteUserRegDetails(RegDetails regdet);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class RegDetails
+    {
+       
+        string p_CPF = string.Empty;
+        string p_Nome = string.Empty;
+        string p_Tipo = string.Empty;
+        string p_Sexo = string.Empty;
+        string p_Situacao = string.Empty;
+
+     
+        [DataMember]
+        public string CPF
+        {
+            get { return p_CPF; }
+            set { p_CPF = value; }
+        }
+        [DataMember]
+        public string Nome
+        {
+            get { return p_Nome; }
+            set { p_Nome = value; }
+        }
+        [DataMember]
+        public string Tipo
+        {
+            get { return p_Tipo; }
+            set { p_Tipo = value; }
+        }
+        [DataMember]
+        public string Sexo
+        {
+            get { return p_Sexo; }
+            set { p_Sexo = value; }
+        }
+        [DataMember]
+        public string Situacao
+        {
+            get { return p_Situacao; }
+            set { p_Situacao = value; }
+        }
+    }
+}
---
+++ WcfServiceGestaoClientes/Service1.svc.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Configuration;
+
+namespace WcfServiceGestaoClientes
+{
+    public class Service1 : IService1
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString);
+
+        public string InsertUserRegDetails(RegDetails regdet)
+        {
+            string Status;
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("insert into Cliente(cpf,nome,tipoCliente,sexo,situacaoCliente) values(@cpf,@nome,@tipoCliente,@sexo,@situacaoCliente)", con);
+            cmd.Parameters.AddWithValue("@cpf", regdet.CPF);
+            cmd.Parameters.AddWithValue("@nome", regdet.Nome);
+            cmd.Parameters.AddWithValue("@tipoCliente", regdet.Tipo);
+            cmd.Parameters.AddWithValue("@sexo", regdet.Sexo);
+            cmd.Parameters.AddWithValue("@situacaoCliente", regdet.Situacao);
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Status = regdet.CPF + " " + regdet.Nome + " registered successfully";
+            }
+            else
+            {
+                Status = regdet.CPF + " " + regdet.Nome + " could not be registered";
+            }
+            con.Close();
+            return Status;
+        }
+
+        public DataSet GetUserRegDetails()
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlCommand cmd = new SqlCommand("Select * from Cliente", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ds;
+        }
+
+        public DataSet FetchUpdatedRecords(RegDetails regdet)
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlCommand cmd = new SqlCommand("select * from Cliente where cpf=@cpf", con);
+
+            cmd.Parameters.AddWithValue("@cpf", regdet.CPF);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ds;
+        }
+
+        public string UpdateUserRegDetails(RegDetails regdet)
+        {
+            string Status;
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlCommand cmd = new SqlCommand("update Cliente set nome=@nome,tipoCliente=@tipoCliente, sexo=@sexo,situacaoCliente=@situacaoCliente where cpf=@cpf", con);
+            cmd.Parameters.AddWithValue("@cpf", regdet.CPF);
+            cmd.Parameters.AddWithValue("@nome", regdet.Nome);
+            cmd.Parameters.AddWithValue("@tipoCliente", regdet.Tipo);
+            cmd.Parameters.AddWithValue("@sexo", regdet.Sexo);
+            cmd.Parameters.AddWithValue("@situacaoCliente", regdet.Situacao);           
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Status = "Record updated successfully";
+            }
+            else
+            {
+                Status = "Record could not be updated";
+            }
+            con.Close();
+            return Status;
+        }
+        public bool DeleteUserRegDetails(RegDetails regdet)
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlCommand cmd = new SqlCommand("delete from Cliente where cpf=@cpf", con);
+
+            cmd.Parameters.AddWithValue("@cpf", regdet.CPF);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return true;
+        }
+    }
+}
